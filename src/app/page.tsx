21e6a96f69e0307971b3c2329ddb6ed8e9f309b3
dturbide <68090159+dturbide@users.noreply.github.com>
2025-04-10 12:02:10 -0400

    Suppression de l'import inutilisé de LogoutLink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import LogoutLink from './LogoutLink'
 
 export default async function LandingPage() {
   const supabase = createServerComponentClient({ cookies })
