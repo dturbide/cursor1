5accf487c2974f08f2e673cc06815d55317c3b9f
dturbide <68090159+dturbide@users.noreply.github.com>
2025-04-11 06:10:28 -0400

    refactor: mise à jour du client Supabase pour utiliser createClientComponentClient

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,13 +1,13 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useEffect, useState } from 'react';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClientComponentClient();
 
 export function useSupabase() {
-  const [supabaseClient] = useState(() => supabase);
+  const [supabaseClient] = useState(() => createClientComponentClient());
 
   useEffect(() => {
     const {
